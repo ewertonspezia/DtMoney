8f0fcba2e1d230a4b9c4ea16108ef3035301a096
ewertonspezia <ewertonspezia@outlook.com>
2021-09-29 12:00:31 -0300

    Initialize TranscationModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModelOpen, setIsNewTransactionModelOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModelOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModelOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModelOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle/>
     </>
   );
